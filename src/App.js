c44ceb839df9ad3b5cfb351b63d3f9d9f270c9ae
syed md furquan <smfurquan7.12@gmail.com>
2021-12-22 13:57:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import navnutshell from './Component/navnutshell';
+
+function App() {
+  return (
+    <div className="App">
+      <navnutshell></navnutshell>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/navnutshell.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./navnutshell.css";
+
+const navnutshell = () => {
+    return (
+        <div>
+            <div className="icon" >icon</div>
+
+            <img
+            style={{cursor:"pointer"}}
+            src=' https://logovectorseek.com/wp-content/uploads/2020/11/nutshell-logo-vector.png'
+            height="80%"
+            alt='logo'>
+                
+            </img>
+        </div>
+    )
+}
+
+export default navnutshell
